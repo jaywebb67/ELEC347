7cf93e370ce57f281e0c29d66a2f5d09f19f85fc
jaywebb67 <66364757+jaywebb67@users.noreply.github.com>
2022-12-13 17:20:35 +0000

    Add goertzel class

---
+++ goertzel_detection/goertzel.hpp
@@ -0,0 +1,68 @@
+#ifndef goertzel_hpp
+#define goertzel_hpp
+
+
+#include "mbed.h"
+
+#define _samplingFrequency 8000
+#define _N 205
+#define PI 3.14
+
+class goertzel {
+
+
+    private:
+        double freq_centres[8] = {697,770,852,941,1209,1336,1477,1633};
+
+        double coeff[8];
+        double Q1[8];
+        double Q2[8];
+
+        void ProcessSample(int sample){
+            float Q0[8];
+            for (int i = 0;i<8;i++){
+                Q0[i] = coeff[i] * Q1[i] - Q2[i] + float(sample);
+                Q2[i] = Q1[i];
+                Q1[i] = Q0[i];
+            }
+        }
+	    void ResetGoertzel(){
+            for (int i = 0;i<8;i++){
+                Q2[i] = 0;
+                Q1[i] = 0;
+            }
+        }
+    public:
+
+        goertzel() {
+            double omega[8];
+            for (int i = 0;i<8;i++){
+                omega[i] = (2.0 * PI * freq_centres[i] ) / _samplingFrequency;
+                coeff[i] = 2.0 * cos(omega[i]);
+            };
+            ResetGoertzel();
+
+        }
+
+	    double* detect(int testData[_N]) {
+            double    *magnitude[8];
+
+            /* Process the samples. */
+            for (int index = 0; index < _N; index++)
+            {
+                ProcessSample(testData[index]);
+            }
+
+            /* Do the "optimized Goertzel" processing. */
+            for (int i = 0;i<8;i++){
+                *magnitude[i] = sqrt(Q1[i]*Q1[i] + Q2[i]*Q2[i] - coeff[i]*Q1[i]*Q2[i]);
+            };
+            ResetGoertzel();
+            return *magnitude;
+
+        };
+        
+
+};
+
+#endif
---
+++ goertzel_detection/main.cpp
@@ -0,0 +1,22 @@
+/* mbed Microcontroller Library
+ * Copyright (c) 2019 ARM Limited
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "mbed.h"
+#include "uopmsb/uop_msb.h"
+#include "goertzel.hpp"
+
+// Blinking rate in milliseconds
+#define Fs     8000
+
+
+int main()
+{
+
+
+
+    while (true) {
+        
+    }
+}
