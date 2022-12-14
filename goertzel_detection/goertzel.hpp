a485a7fb0e988b99dbf571ca115d38b0d505069c
jaywebb67 <66364757+jaywebb67@users.noreply.github.com>
2022-12-14 19:09:29 +0000

    displaying magnitudes

--- goertzel_detection/goertzel.hpp
+++ goertzel_detection/goertzel.hpp
@@ -3,6 +3,8 @@
 
 
 #include "mbed.h"
+#include <cstdint>
+#include "math.h"
 
 #define _samplingFrequency 8000
 #define _N 200
@@ -11,58 +13,59 @@
 class goertzel {
 
 
+
+
     private:
         double freq_centres[8] = {697,770,852,941,1209,1336,1477,1633};
 
-        double coeff[8];
-        double Q1[8];
-        double Q2[8];
+        double _targetFrequency;
+        double coeff;
+        double Q1;
+        double Q2;
+        double Q0;
+        double omega;
 
         void ProcessSample(int sample){
-            float Q0[8];
-            for (int i = 0;i<8;i++){
-                Q0[i] = coeff[i] * Q1[i] - Q2[i] + float(sample);
-                Q2[i] = Q1[i];
-                Q1[i] = Q0[i];
-            }
+
+                Q0 = coeff * Q1 - Q2 + float(sample);
+                Q2 = Q1;
+                Q1 = Q0;
+            
         }
 	    void ResetGoertzel(){
-            for (int i = 0;i<8;i++){
-                Q2[i] = 0;
-                Q1[i] = 0;
-            }
+            Q2 = 0;
+            Q1 = 0;
+            Q0 = 0;
         }
+
     public:
 
-        goertzel() {
-            double omega[8];
-            for (int i = 0;i<8;i++){
-                omega[i] = (2.0 * PI * freq_centres[i] ) / _samplingFrequency;
-                coeff[i] = 2.0 * cos(omega[i]);
-            };
+
+        goertzel(double targetFrequency) : _targetFrequency(targetFrequency){
+
+                omega = (2.0 * PI * _targetFrequency ) / _samplingFrequency;
+                coeff = 2.0 * cos(omega);
+
             ResetGoertzel();
 
         }
 
-	    double* detect(int testData[_N]) {
-            double    *magnitude[8];
-
+	    double detect(volatile int16_t testData[_N]) {
+            double magnitude;
+            ResetGoertzel();
             /* Process the samples. */
-            for (int index = 0; index < _N; index++)
+            for (uint16_t index = 0; index < _N; index++)
             {
                 ProcessSample(testData[index]);
             }
 
             /* Do the "optimized Goertzel" processing. */
-            for (int i = 0;i<8;i++){
-                *magnitude[i] = sqrt(Q1[i]*Q1[i] + Q2[i]*Q2[i] - coeff[i]*Q1[i]*Q2[i]);
-            };
-            ResetGoertzel();
-            return *magnitude;
+            return magnitude = sqrt(Q1*Q1 + Q2*Q2 - coeff*Q1*Q2);
+
+
+            
 
         };
-        
-
 };
 
 #endif
--- goertzel_detection/main.cpp
+++ goertzel_detection/main.cpp
@@ -7,40 +7,85 @@
 #include "uopmsb/uop_msb.h"
 #include "goertzel.hpp"
 #include <cstdint>
+#include <cstdio>
 #include <stdint.h>
 
 // Blinking rate in milliseconds
 #define Fs     8000
-// #define mic_pin PA_3
 
 osThreadId_t mainThreadID;
+Thread calcThread,outputThread;
+
+Mutex bufferLock;
 
 AnalogIn dataIn(MIC_AN_PIN);
 Ticker sampleTick;
 
+volatile int16_t bufferB[200];
+
 void sampleISR();
 
+void calcFreqMag(){
+
+    goertzel lf1(697);
+    goertzel lf2(770);
+    goertzel lf3(852);
+    goertzel lf4(941);
+
+    goertzel hf1(1209);
+    goertzel hf2(1336);
+    goertzel hf3(1477);
+    goertzel hf4(1633);
+    while(true){
+
+        if(!ThisThread::flags_wait_any_for(2,1s)){
+            system_reset();
+        }
+        if(!bufferLock.trylock_for(1s)){
+            system_reset();
+        }
+        uint32_t lfmag1 = lf1.detect(bufferB);
+        uint32_t lfmag2 = lf2.detect(bufferB);
+        uint32_t lfmag3 = lf3.detect(bufferB);
+        uint32_t lfmag4 = lf4.detect(bufferB);
+        uint32_t hfmag1 = hf1.detect(bufferB);
+        uint32_t hfmag2 = hf2.detect(bufferB);
+        uint32_t hfmag3 = hf3.detect(bufferB);
+        uint32_t hfmag4 = hf4.detect(bufferB);
+        printf("%u\t%u\t%u\t%u\t%u\t%u\t%u\t%u\t",lfmag1,lfmag2,lfmag3,lfmag4,hfmag1,hfmag2,hfmag3,hfmag4);
+        bufferLock.unlock();
+        printf("\n\n\n");
+        ThisThread::flags_clear(2); 
+    }
+
+}
 
 int main() {
-    int idx = 0;
+    uint16_t idx = 0;
     int16_t bufferA;
-    int16_t bufferB[200];
-    goertzel data;
 
+
+    calcThread.start(calcFreqMag);
     sampleTick.attach(&sampleISR,26ms);
 
     mainThreadID = ThisThread::get_id();
     while (true) {
-        ThisThread::flags_wait_any(1);
-        
-        for (int i =0;i<200;i++){
-            bufferA = dataIn.read_u16()-0x8000;
-            bufferB[idx] = bufferA;
-            wait_us(125);
+        if(!ThisThread::flags_wait_any_for(1,1s)){
+            system_reset();
         }
-        
+        if(!bufferLock.trylock_for(1s)){
+            system_reset();
+        }
+        for (uint16_t i =0;i<200;i++){
+            bufferA = dataIn.read_u16()-0x8000;
+            bufferB[i] = bufferA;
+            wait_us(125);
+        };
+        bufferLock.unlock();
         ThisThread::flags_clear(1);
-        printf("\n\n\n\n");
+        calcThread.flags_set(2);
+
+
 
         
 
