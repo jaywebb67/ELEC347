773ed5f98f24794221bf72870ea117a88f060ece
jaywebb67 <66364757+jaywebb67@users.noreply.github.com>
2022-12-17 19:17:00 +0000

    7 segment working

--- goertzel_detection/main.cpp
+++ goertzel_detection/main.cpp
@@ -7,10 +7,11 @@
 #include "uopmsb/uop_msb.h"
 #include "goertzel.hpp"
 
+using namespace uop_msb;
 
+#define threshold 150000
 
-
-
+LatchedLED sev_seg(LatchedLED::SEVEN_SEG,LatchedLED::UNITS);
 
 int main() {
 
@@ -20,13 +21,74 @@ int main() {
      
         data.sample();
         data.detect();
-        for (int i = 0; i<8; i++){
 
-            printf("%lf\t",data.magnitude[i]);
-        };
-
+        if ((data.magnitude[0]>threshold)&(data.magnitude[1]>threshold)&(data.magnitude[4]>threshold)){
+            sev_seg.enable(1);
+            sev_seg = 1;
+        }
+        else if ((data.magnitude[1]>threshold)&(data.magnitude[2]>threshold)&(data.magnitude[4]>threshold)) {
+            sev_seg.enable(1);
+            sev_seg = 4;
+        }
+        else if ((data.magnitude[2]>threshold)&(data.magnitude[3]>threshold)&(data.magnitude[4]>threshold)) {
+            sev_seg.enable(1);
+            sev_seg = 7;
+        }   
+        else if ((data.magnitude[2]<threshold)&(data.magnitude[3]>threshold)&(data.magnitude[4]>threshold)) {
+            sev_seg.enable(1);
+            sev_seg = 10;
+        } 
+        else if ((data.magnitude[0]>threshold)&(data.magnitude[1]>threshold)&(data.magnitude[5]>threshold)) {
+            sev_seg.enable(1);
+            sev_seg = 2;
+        } 
+        else if ((data.magnitude[1]>threshold)&(data.magnitude[2]>threshold)&(data.magnitude[5]>threshold)) {
+            sev_seg.enable(1);
+            sev_seg = 5;
+        } 
+        else if ((data.magnitude[2]>threshold)&(data.magnitude[3]>threshold)&(data.magnitude[5]>threshold)) {
+            sev_seg.enable(1);
+            sev_seg = 8;
+        } 
+        else if ((data.magnitude[2]<threshold)&(data.magnitude[3]>threshold)&(data.magnitude[5]>threshold)) {
+            sev_seg.enable(1);
+            sev_seg = 0;
+        } 
+        else if ((data.magnitude[0]>threshold)&(data.magnitude[1]>threshold)&(data.magnitude[6]>threshold)) {
+            sev_seg.enable(1);
+            sev_seg = 3;
+        } 
+        else if ((data.magnitude[1]>threshold)&(data.magnitude[2]>threshold)&(data.magnitude[6]>threshold)) {
+            sev_seg.enable(1);
+            sev_seg = 6;
+        } 
+        else if ((data.magnitude[2]>threshold)&(data.magnitude[3]>threshold)&(data.magnitude[6]>threshold)) {
+            sev_seg.enable(1);
+            sev_seg = 9;
+        } 
+        else if ((data.magnitude[2]<threshold)&(data.magnitude[3]>threshold)&(data.magnitude[6]>threshold)) {
+            sev_seg.enable(1);
+            sev_seg = 11;
+        }
+        else if ((data.magnitude[0]>threshold)&(data.magnitude[1]>threshold)&(data.magnitude[7]>threshold)) {
+            sev_seg.enable(1);
+            sev_seg = 12;
+        } 
+        else if ((data.magnitude[1]>threshold)&(data.magnitude[2]>threshold)&(data.magnitude[7]>threshold)) {
+            sev_seg.enable(1);
+            sev_seg = 13;
+        } 
+        else if ((data.magnitude[2]>threshold)&(data.magnitude[3]>threshold)&(data.magnitude[7]>threshold)) {
+            sev_seg.enable(1);
+            sev_seg = 14;
+        } 
+        else if ((data.magnitude[2]<threshold)&(data.magnitude[3]>threshold)&(data.magnitude[7]>threshold)) {
+            sev_seg.enable(1);
+            sev_seg = 15;
+        }
 
         wait_us(1000000);
+        sev_seg.enable(0);
     }
 };
 
